c3bdfc636ba4c2f1486e8ae5f39c9e18e96a2721
lukit <lukkitaalves6@gmail.com>
2024-05-18 00:20:39 -0300

    feat - Adiciona testes unitários

--- src/test/java/com/fiap/pagamentos_ms_techchallenge/data/adapter/PagamentoAdapterTest.java
+++ src/test/java/com/fiap/pagamentos_ms_techchallenge/data/adapter/PagamentoAdapterTest.java
@@ -4,23 +4,28 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fiap.pagamentos_ms_techchallenge.data.entities.PagamentoEntity;
 import com.fiap.pagamentos_ms_techchallenge.data.repositories.PagamentoJpaRepository;
 import com.fiap.pagamentos_ms_techchallenge.domain.enums.PagamentoStatus;
 import com.fiap.pagamentos_ms_techchallenge.domain.models.PagamentoModel;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class PagamentoAdapterTest {
 
     @Mock
@@ -32,42 +37,70 @@ public class PagamentoAdapterTest {
     @InjectMocks
     private PagamentoAdapter pagamentoAdapter;
 
+    private ObjectMapper objectMapper;
+
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.openMocks(this);
+        objectMapper = new ObjectMapper();
+        pagamentoAdapter = new PagamentoAdapter(pagamentoJpaRepository);
     }
 
     @Test
     public void testSalvar() throws Exception {
-
         PagamentoModel pagamentoModel = new PagamentoModel();
-        pagamentoModel.setPedidoId("123");
+        pagamentoModel.setPedidoId(String.valueOf(1L));
         pagamentoModel.setTotal(100.0F);
 
+        Map<String, String> responseBody = new LinkedHashMap<>();
+        responseBody.put("qr_data", "some-qr-code");
+
         PagamentoEntity pagamentoEntity = new PagamentoEntity();
-        pagamentoEntity.setPedidoId("123");
+        pagamentoEntity.setPedidoId(String.valueOf(1L));
         pagamentoEntity.setTotal(100.0F);
         pagamentoEntity.setDataPagamentoGerado(LocalDateTime.now());
         pagamentoEntity.setStatusPagamento(PagamentoStatus.CRIADO.toString());
-        pagamentoEntity.setQrCode("mockedQRCode");
+        pagamentoEntity.setQrCode("some-qr-code");
 
         when(pagamentoJpaRepository.save(any(PagamentoEntity.class))).thenReturn(pagamentoEntity);
 
-
-        Map<String, String> responseMap = new HashMap<>();
-        responseMap.put("qr_data", "mockedQRCode");
-        ResponseEntity<Object> responseEntity = new ResponseEntity<>(responseMap, HttpStatus.OK);
-
-        when(restTemplate.exchange(anyString(), eq(HttpMethod.POST), any(HttpEntity.class), eq(Object.class)))
-                .thenReturn(responseEntity);
-
-
         PagamentoModel result = pagamentoAdapter.salvar(pagamentoModel);
 
+        assertEquals("some-qr-code", result.getQrCode());
+        assertEquals(String.valueOf(1L), result.getPedidoId());
+        assertEquals(PagamentoStatus.CRIADO.toString(), result.getStatusPagamento());
 
-        assertEquals(pagamentoEntity.getId(), result.getId());
-        assertEquals(pagamentoEntity.getDataPagamentoGerado(), result.getDataPagamentoGerado());
-        assertEquals(pagamentoEntity.getStatusPagamento(), result.getStatusPagamento());
-        assertEquals(pagamentoEntity.getQrCode(), result.getQrCode());
+        verify(pagamentoJpaRepository, times(1)).save(any(PagamentoEntity.class));
+    }
+
+    @Test
+    public void testListaPagamentosAtivos() {
+        PagamentoEntity pagamentoEntity = new PagamentoEntity();
+        pagamentoEntity.setPedidoId(String.valueOf(1L));
+        pagamentoEntity.setTotal(100.0F);
+
+        when(pagamentoJpaRepository.findAllPagamentosAtivos()).thenReturn(Collections.singletonList(pagamentoEntity));
+
+        List<PagamentoEntity> result = pagamentoAdapter.listaPagamentosAtivos();
+
+        assertEquals(1, result.size());
+        assertEquals(String.valueOf(1L), result.get(0).getPedidoId());
+
+        verify(pagamentoJpaRepository, times(1)).findAllPagamentosAtivos();
+    }
+
+    @Test
+    public void testListaPagamentosAll() {
+        PagamentoEntity pagamentoEntity = new PagamentoEntity();
+        pagamentoEntity.setPedidoId("aaa");
+        pagamentoEntity.setTotal(100.0F);
+
+        when(pagamentoAdapter.listaPagamentosAtivos()).thenReturn(Collections.singletonList(pagamentoEntity));
+
+        List<PagamentoEntity> result = pagamentoAdapter.listaPagamentosAtivos();
+
+        assertEquals(1, result.size());
+        assertEquals("aaa", result.get(0).getPedidoId());
+
+        verify(pagamentoJpaRepository, times(1)).findAllPagamentosAtivos();
     }
 }
