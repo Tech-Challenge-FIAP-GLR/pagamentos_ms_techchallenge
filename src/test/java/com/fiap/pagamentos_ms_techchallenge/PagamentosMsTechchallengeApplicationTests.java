48ed63e22a466dda0e7126e7b0846cb8c171d382
lukit <lukkitaalves6@gmail.com>
2024-05-18 00:34:29 -0300

    feat - Adiciona testes unitários 2

--- src/test/java/com/fiap/pagamentos_ms_techchallenge/PagamentosMsTechchallengeApplicationTests.java
+++ src/test/java/com/fiap/pagamentos_ms_techchallenge/PagamentosMsTechchallengeApplicationTests.java
@@ -1,9 +1,19 @@
 package com.fiap.pagamentos_ms_techchallenge;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PagamentosMsTechchallengeApplicationTests {
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class PagamentosMsTechchallengeApplicationTests {
+
+    @Test
+    void testAnnotations() {
+        assertTrue(PagamentosMsTechchallengeApplication.class.isAnnotationPresent(SpringBootApplication.class));
+        assertTrue(PagamentosMsTechchallengeApplication.class.isAnnotationPresent(EnableScheduling.class));
+    }
 }
